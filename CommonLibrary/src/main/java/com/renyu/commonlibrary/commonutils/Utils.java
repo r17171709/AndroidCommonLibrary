3478703bef013ec2ef00694ef8225325354094f7
renyu <Yuren2@hotmail.com>
2017-08-03 17:37:06 +0800

    第三方框架升级

--- CommonLibrary/src/main/java/com/renyu/commonlibrary/commonutils/Utils.java
+++ CommonLibrary/src/main/java/com/renyu/commonlibrary/commonutils/Utils.java
@@ -188,35 +188,6 @@ public class Utils {
     }
 
     /**
-     * 计算距离
-     * @param num
-     * @return
-     */
-    public static String caculateDistance(int num) {
-        if (num<1000) {
-            return num+"m";
-        }
-        else {
-            return num/1000+"km";
-        }
-    }
-
-    /**
-     * 去除小数点后一位是0的情况
-     * @param number
-     * @return
-     */
-    public static String removeZero(String number) {
-        double num=Double.parseDouble(number);
-        if (num==(int) num) {
-            return ""+(int) num;
-        }
-        else {
-            return ""+num;
-        }
-    }
-
-    /**
      * 获取view截图
      * @param view
      * @return
