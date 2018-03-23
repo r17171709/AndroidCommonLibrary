023329ef0926ed00ffc9c483dbb6c5e2947b75b4
renyu <Yuren2@hotmail.com>
2018-03-23 10:18:12 +0800

    新增Fragment中的Context默认参数

--- CommonLibrary/src/main/java/com/renyu/commonlibrary/basefrag/BaseFragment.java
+++ CommonLibrary/src/main/java/com/renyu/commonlibrary/basefrag/BaseFragment.java
@@ -1,5 +1,9 @@
 package com.renyu.commonlibrary.basefrag;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -25,6 +29,29 @@ public abstract class BaseFragment extends Fragment {
 
     public OKHttpHelper httpHelper;
     public Retrofit retrofit=null;
+
+    // 不需要再getActivity()了
+    public Context context;
+
+    @TargetApi(23)
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        onAttachToContext(context);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            onAttachToContext(activity);
+        }
+    }
+
+    protected void onAttachToContext(Context context) {
+        this.context = context;
+    }
 
     @Nullable
     @Override
