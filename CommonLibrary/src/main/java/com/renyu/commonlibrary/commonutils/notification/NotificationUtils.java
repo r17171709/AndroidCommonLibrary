89a4ef5439aaa8f368c47c43c00d1ebc16f2acdf
r17171709 <Yuren2@hotmail.com>
2019-01-10 15:54:15 +0800

    修改通知权限判断功能

--- CommonLibrary/src/main/java/com/renyu/commonlibrary/commonutils/notification/NotificationUtils.java
+++ CommonLibrary/src/main/java/com/renyu/commonlibrary/commonutils/notification/NotificationUtils.java
@@ -550,29 +550,34 @@ public class NotificationUtils {
      */
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public static boolean isNotificationEnabled() {
-        String CHECK_OP_NO_THROW = "checkOpNoThrow";
-        String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            String CHECK_OP_NO_THROW = "checkOpNoThrow";
+            String OP_POST_NOTIFICATION = "OP_POST_NOTIFICATION";
 
-        AppOpsManager mAppOps = (AppOpsManager) Utils.getApp().getSystemService(Context.APP_OPS_SERVICE);
-        ApplicationInfo appInfo = Utils.getApp().getApplicationInfo();
-        String pkg = Utils.getApp().getPackageName();
-        int uid = appInfo.uid;
+            AppOpsManager mAppOps = (AppOpsManager) Utils.getApp().getSystemService(Context.APP_OPS_SERVICE);
+            ApplicationInfo appInfo = Utils.getApp().getApplicationInfo();
+            String pkg = Utils.getApp().getPackageName();
+            int uid = appInfo.uid;
 
-        Class appOpsClass;
-        /* Context.APP_OPS_MANAGER */
-        try {
-            appOpsClass = Class.forName(AppOpsManager.class.getName());
-            Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE, Integer.TYPE,
-                    String.class);
-            Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
+            Class appOpsClass;
+            /* Context.APP_OPS_MANAGER */
+            try {
+                appOpsClass = Class.forName(AppOpsManager.class.getName());
+                Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE, Integer.TYPE,
+                        String.class);
+                Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
 
-            int value = (Integer) opPostNotificationValue.get(Integer.class);
-            return ((Integer) checkOpNoThrowMethod.invoke(mAppOps, value, uid, pkg) == AppOpsManager.MODE_ALLOWED);
+                int value = (Integer) opPostNotificationValue.get(Integer.class);
+                return ((Integer) checkOpNoThrowMethod.invoke(mAppOps, value, uid, pkg) == AppOpsManager.MODE_ALLOWED);
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
         }
-        return false;
+        else {
+            return NotificationManagerCompat.from(Utils.getApp()).areNotificationsEnabled();
+        }
     }
 
     /**
