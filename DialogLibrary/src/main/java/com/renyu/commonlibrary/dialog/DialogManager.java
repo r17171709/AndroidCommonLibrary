e90029a5fe26cfc868c510950e9e5a7f87f484fc
renyu <Yuren2@hotmail.com>
2019-04-04 13:09:37 +0800

    新增Dialog连续弹出管理类

---
+++ DialogLibrary/src/main/java/com/renyu/commonlibrary/dialog/DialogManager.java
@@ -0,0 +1,62 @@
+package com.renyu.commonlibrary.dialog;
+
+import android.support.v4.app.FragmentActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class DialogManager {
+    private static volatile DialogManager manager;
+    private static ArrayList<HashMap<FragmentActivity, ChoiceDialog>> lists;
+
+    public static DialogManager getInstance() {
+        if (manager == null) {
+            synchronized (DialogManager.class) {
+                if (manager == null) {
+                    manager = new DialogManager();
+                    lists = new ArrayList<>();
+                }
+            }
+        }
+        return manager;
+    }
+
+    /**
+     * 添加Dialog
+     * @param fragmentActivity
+     * @param choiceDialog
+     */
+    public synchronized void addDialog(FragmentActivity fragmentActivity, ChoiceDialog choiceDialog) {
+        HashMap<FragmentActivity, ChoiceDialog> map = new HashMap<>();
+        choiceDialog.setOnDialogDismissListener(() -> {
+            lists.remove(map);
+            if (lists.size() != 0) {
+                showDialog(lists.get(0));
+            }
+        });
+        map.put(fragmentActivity, choiceDialog);
+        if (lists.size() == 0) {
+            lists.add(map);
+            showDialog(map);
+        } else {
+            lists.add(map);
+        }
+    }
+
+    private static void showDialog(HashMap<FragmentActivity, ChoiceDialog> map) {
+        FragmentActivity fragmentActivity = null;
+        ChoiceDialog choiceDialog = null;
+
+        Iterator<Map.Entry<FragmentActivity, ChoiceDialog>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<FragmentActivity, ChoiceDialog> entry = it.next();
+            fragmentActivity = entry.getKey();
+            choiceDialog = entry.getValue();
+        }
+        if (fragmentActivity != null && choiceDialog != null) {
+            choiceDialog.show(fragmentActivity);
+        }
+    }
+}
