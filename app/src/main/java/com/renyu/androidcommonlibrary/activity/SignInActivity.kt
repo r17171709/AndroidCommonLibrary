8420265e75fb77b43bf0916fc300b22f80820659
r17171709 <Yuren2@hotmail.com>
2019-05-22 11:50:08 +0800

    登录页范例调整

--- app/src/main/java/com/renyu/androidcommonlibrary/activity/SignInActivity.kt
+++ app/src/main/java/com/renyu/androidcommonlibrary/activity/SignInActivity.kt
@@ -2,12 +2,18 @@ package com.renyu.androidcommonlibrary.activity
 
 import android.graphics.Color
 import android.graphics.Rect
+import android.view.View
 import android.widget.FrameLayout
-import com.blankj.utilcode.util.BarUtils as BarUtils1
-import com.renyu.commonlibrary.commonutils.BarUtils as BarUtils2
+import com.blankj.utilcode.util.ToastUtils
+import com.jakewharton.rxbinding3.view.clicks
+import com.jakewharton.rxbinding3.widget.textChanges
 import com.renyu.androidcommonlibrary.R
 import com.renyu.commonlibrary.baseact.BaseActivity
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_signin.*
+import java.util.concurrent.TimeUnit
+import com.blankj.utilcode.util.BarUtils as BarUtils1
+import com.renyu.commonlibrary.commonutils.BarUtils as BarUtils2
 
 class SignInActivity : BaseActivity() {
     private var previousKeyboardHeight = -1
@@ -25,8 +31,7 @@ class SignInActivity : BaseActivity() {
                 val hide = displayHeight.toDouble() / decorViewHeight > 0.8
                 if (hide) {
                     rootView.scrollTo(0, 0)
-                }
-                else {
+                } else {
                     btn_signin.post {
                         rootView.scrollBy(0, keyboardHeight - (decorViewHeight - btn_signin.bottom))
                     }
@@ -38,9 +43,36 @@ class SignInActivity : BaseActivity() {
 
     override fun initViews() = R.layout.activity_signin
 
-    override fun loadData() {}
+    override fun loadData() {
+        val textChanges = arrayOf(
+                tv_signin_uname.textChanges(),
+                tv_signin_password.textChanges()
+        )
+        Observable.combineLatest(textChanges) {
+            it.all { value ->
+                value.toString().isNotEmpty()
+            }
+        }.subscribe {
+            if (it) {
+                ToastUtils.showShort("可以点击")
+            } else {
+                ToastUtils.showShort("不可点击")
+            }
+        }
+        btn_signin.apply {
+            onAntiShakeClick {
+                ToastUtils.showShort("点击了")
+            }
+        }
+    }
 
     override fun setStatusBarColor() = Color.BLACK
 
     override fun setStatusBarTranslucent() = 0
+}
+
+fun View.onAntiShakeClick(block: (View) -> Unit) {
+    clicks().throttleFirst(2, TimeUnit.SECONDS).subscribe {
+        block.invoke(this)
+    }
 }
