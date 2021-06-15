df2782551e07987f73983bcc42128c40f22e9730
renyu <Yuren2@hotmail.com>
2021-06-15 17:51:30 +0800

    完善获取页面传递参数公共方法

--- CommonLibrary/src/main/java/com/renyu/commonlibrary/commonutils/Extensions.kt
+++ CommonLibrary/src/main/java/com/renyu/commonlibrary/commonutils/Extensions.kt
@@ -1,5 +1,7 @@
 package com.renyu.commonlibrary.commonutils
 
+import android.app.Activity
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -37,3 +39,64 @@ inline fun <reified VB : ViewBinding> AppCompatActivity.binding() =
         .invoke(null, layoutInflater) as VB).apply {
         setContentView(root)
     }
+
+
+// 获取传递参数
+fun <U, T> bindStringExtra(key: String) = DataStringDelegate<U, T>(key)
+
+fun <U, T> bindStringArgument(key: String) = DataStringDelegate<U, T>(key)
+
+fun <U, T> bindIntExtra(key: String) = DataStringDelegate<U, T>(key)
+
+fun <U, T> bindIntArgument(key: String) = DataStringDelegate<U, T>(key)
+
+fun <U, T> bindSerializableExtra(key: String) = DataSerializableDelegate<U, T>(key)
+
+fun <U, T> bindSerializableArgument(key: String) = DataSerializableDelegate<U, T>(key)
+
+fun <U, T : Parcelable?> bindParcelableExtra(key: String) =
+    DataParcelableDelegate<U, T?>(key)
+
+fun <U, T : Parcelable> bindParcelableArgument(key: String) =
+    DataParcelableDelegate<U, T>(key)
+
+class DataStringDelegate<U, T>(private val params: String) : ReadOnlyProperty<U, T?> {
+    override fun getValue(thisRef: U, property: KProperty<*>): T? {
+        return when (thisRef) {
+            is Activity -> thisRef.intent.getStringExtra(params) as T?
+            is Fragment -> thisRef.arguments?.getString(params) as T?
+            else -> null
+        }
+    }
+}
+
+class DataIntDelegate<U, T>(private val params: String) : ReadOnlyProperty<U, T?> {
+    override fun getValue(thisRef: U, property: KProperty<*>): T? {
+        return when (thisRef) {
+            is Activity -> thisRef.intent.getIntExtra(params, -1) as T?
+            is Fragment -> thisRef.arguments?.getInt(params) as T?
+            else -> null
+        }
+    }
+}
+
+class DataSerializableDelegate<U, T>(private val params: String) : ReadOnlyProperty<U, T?> {
+    override fun getValue(thisRef: U, property: KProperty<*>): T? {
+        return when (thisRef) {
+            is Activity -> thisRef.intent.getSerializableExtra(params) as T?
+            is Fragment -> thisRef.arguments?.getSerializable(params) as T?
+            else -> null
+        }
+    }
+}
+
+class DataParcelableDelegate<U, T : Parcelable?>(private val params: String) :
+    ReadOnlyProperty<U, T?> {
+    override fun getValue(thisRef: U, property: KProperty<*>): T? {
+        return when (thisRef) {
+            is Activity -> thisRef.intent.getParcelableExtra<T>(params)
+            is Fragment -> thisRef.arguments?.getParcelable<T>(params)
+            else -> null
+        }
+    }
+}
---
+++ app/src/main/java/com/renyu/androidcommonlibrary/activity/MyX5WebActivity.kt
@@ -0,0 +1,90 @@
+package com.renyu.androidcommonlibrary.activity
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.TextView
+import com.renyu.androidcommonlibrary.R
+import com.renyu.androidcommonlibrary.impl.X5WebAppInterface
+import com.renyu.commonlibrary.commonutils.bindParcelableExtra
+import com.renyu.commonlibrary.commonutils.bindStringExtra
+import com.renyu.commonlibrary.web.activity.X5WebActivity
+
+class MyX5WebActivity : X5WebActivity() {
+    private val impl: X5WebAppInterface? by bindParcelableExtra<X5WebActivity, X5WebAppInterface>("IWebApp")
+    private val iWebAppName: String? by bindStringExtra<X5WebActivity, String>("IWebAppName")
+
+    // 文件上传使用
+    private var uploadFilePathCallback: ValueCallback<Array<Uri>>? = null
+
+    override fun getTitleView() = findViewById<TextView>(R.id.tv_nav_title)
+
+    override fun getNavClose() = findViewById<ImageButton>(R.id.ib_nav_close)
+
+    override fun getNavBack() = findViewById<ImageButton>(R.id.ib_nav_left)
+
+    override fun getRootView() = findViewById<FrameLayout>(R.id.layout_x5webview)
+
+    override fun onPageFinished(url: String) {}
+
+    override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+        // 判断是否需要拦截
+//        if (url.contains("weixin")) {
+//            return true;
+//        }
+        return false
+    }
+
+    override fun onShowFileChooser(
+        webView: WebView,
+        filePathCallback: ValueCallback<Array<Uri>>,
+        fileChooserParams: WebChromeClient.FileChooserParams
+    ): Boolean {
+        uploadFilePathCallback = filePathCallback
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "image/*"
+        startActivityForResult(intent, 10)
+        return true
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_x5web)
+        navClose.setImageResource(R.mipmap.ic_web_close)
+        navBack.setImageResource(R.mipmap.ic_arrow_black_left)
+        initViews()
+        if (impl != null) {
+            impl!!.setContext(this)
+            impl!!.setWebView(webView)
+            webView.addJavascriptInterface(impl!!, iWebAppName!!)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 10 && resultCode == RESULT_OK) {
+            if (null == uploadFilePathCallback) {
+                return
+            }
+            uploadFilePathCallback!!.onReceiveValue(
+                WebChromeClient.FileChooserParams.parseResult(
+                    resultCode,
+                    data
+                )
+            )
+            uploadFilePathCallback = null
+        } else if (resultCode == RESULT_CANCELED) {
+            // 没有选择图片要重置
+            if (uploadFilePathCallback != null) {
+                uploadFilePathCallback!!.onReceiveValue(null)
+                uploadFilePathCallback = null
+            }
+        }
+    }
+}
